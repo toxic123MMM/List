7db43b8b52fb857e6772383db498e5565d29815b
toxic123MMM <aashish02@gmail.com>
2024-12-16 17:33:47 +0400

    Add files via upload

---
+++ List.py
@@ -0,0 +1,13 @@
+empty_list=[]
+print(empty_list)
+
+#list of numbers
+numbers=[36,48,62,93]
+print(numbers)
+
+num=[36,48,62,93]*3
+print(num)
+
+num_list=[36,48,62,93]
+num_list=num_list[::-1]
+print(num_list)
